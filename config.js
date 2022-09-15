bdfdc6637e53e7049ad4ce0432775d1dcf6089aa
NCDev <55439243+xenfovn@users.noreply.github.com>
2022-09-15 08:23:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,43 +1,18 @@
 module.exports = {
     defaultWhitelistIps: [
-        "201.42.94.22",
-        "146.59.193.195",
-        "51.210.126.15",
-        "51.178.63.83",
-        "178.33.224.212",
-        "5.135.143.71",
-        "121.4.163.148"
+        "1.1.1.1",
     ],
-    serverIP: "20.198.111.120",
+    serverIP: "103.82.22.25",
     servers: [
         {
-            id: "s1fivemshield",
-            domain: "fivemshield.xyz",
+            id: "c1cdevs",
+            domain: "c1.cdevs.xyz",
             default: true,
-            name: "fivemshield Online",
+            name: "CDEVS PROTECT",
             reSecret: "",
             reSiteKey: "",
-            fivemIP: "52.188.112.219",
+            fivemIP: "103.98.151.95",
             fivemPort: 30120
-        },
-        {
-            id: "fivemshield",
-            domain: "s1.fivemshield.xyz",
-            name: "fivemshield Online",
-            reSecret: "",
-            reSiteKey: "",
-            fivemIP: "52.188.112.219",
-            fivemPort: 30121
-        },
-        // {
-        //     id: "default",
-        //     default: true,          // Fallback all other domains to this config
-        //     domain: "default.xyz",
-        //     name: "default Online",
-        //     reSecret: "",
-        //     reSiteKey: "",
-        //     fivemIP: "52.188.112.219",
-        //     fivemPort: 30127
-        // }
+        }
     ]
 }
